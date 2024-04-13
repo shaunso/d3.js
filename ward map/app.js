718c669ee878f193c72cd3c031521a95e6d65ba5
shaunso <shaunsom@gmail.com>
2024-04-13 21:18:53 +0200

    Add files via upload

--- ward map/app.js
+++ ward map/app.js
@@ -7,9 +7,6 @@ async function draw() {
   // variable is called in the next block, which is an if statement
   // default option shall be _population density_
   const data = await d3.csv("wardData.csv");
-
-
-  // const administrativeLevel = "district"
   
   ///////////////////
   // setting the dimensions for the map area
@@ -47,9 +44,9 @@ async function draw() {
   const domain = [0.1, Math.max(...data.map(d => d.POPULATION_DENSITY))];
 
   const color =  d3.scaleLog()
-      .base(2)
-      .domain(domain)
-      .interpolate(() => d3.interpolatePlasma)
+    .base(2)
+    .domain(domain)
+    .interpolate(() => d3.interpolatePlasma);
 
   // drawing the map on the web page
   chart.selectAll("path")
@@ -59,8 +56,16 @@ async function draw() {
     .attr("fill", d => color(valueMap.get(d.properties.ADM3_PCODE)))
     .on("click", function(d, i) {
       console.log(`The population density for ${i.properties.ADM2_EN} ward ${i.properties.ADM3_EN} is ${d3.format(",")(valueMap.get(i.properties.ADM3_PCODE))} people per square kilometer`);
-    }
-    )
+    });
+  
+/////////////////////////////////
+// renders the provincial boundaries
+    // chart.append("path")
+    //   .datum(topojson.mesh(provinceTopology, provinceTopology.objects.province, (a, b) => a !== b))
+    //   .attr("fill", "none")
+    //   .attr("stroke", "white")
+    //   .attr("stroke-linejoin", "round")
+    //   .attr("d", pathGenerator);
 
   ///////////////////////////////////////////////
     // zoom function
@@ -73,17 +78,56 @@ async function draw() {
   // Then we instruct the computer, when it “sees” a “zoom” event, to carry out another function (.on("zoom",function(event) {).
   // 
   // that function firstly selects all our paths g.selectAll('path') and then applies a transform to them based on the event (.attr('tran
-// 
-const zoom = d3.zoom()
-.scaleExtent([1, 20])
-.on('zoom', function(event) {
-  chart.selectAll('path')
-    .attr('transform', event.transform)
-});
+  // 
+  const zoom = d3.zoom()
+    .scaleExtent([1, 20])
+    .on('zoom', function(event) {
+      chart.selectAll('path')
+        .attr('transform', event.transform)
+    });
 
-// Calling the zoom function.
-svg.call(zoom);
-    
+  // Calling the zoom function.
+  svg.call(zoom);
+
+  ///////////////////////////////////////////////
+  // legend
+  ///////////////////////////////////////////////
+  // render legend
+  
+  const thresholds = d3.range(-3, 16).map(x => 2 ** x);
+  
+  const x = d3.scaleLog()
+    .base(2)
+    .domain(domain)
+    .rangeRound([0, 260]);
+
+  const legend = chart.append("g")
+      .style("font-size", "0.8rem")
+      .style("font-family", "sans-serif")
+      .attr("transform", "translate(20,40)");
+  
+  const label = legend.append("text")
+      .style("font-weight", "bold")
+      .text("Population density (persons / km²)")
+      .attr("transform", "translate( 198, -8)");
+  
+  const scale = legend.append("g");
+  
+  scale.selectAll("rect")
+    .data(thresholds)
+    .join("rect")
+      .attr("height", 10)
+      .attr("x", d => x(d))
+      .attr("width", d => x(2 * d) - x(d))
+      .attr("fill", d => color(d));
+  
+  scale.call(
+    d3.axisBottom(x)
+      .tickSize(15)
+      .tickFormat(v => v > 9999 ? d3.format(".2s")(v) : v > 999 ? d3.format(".1s")(v) : String(v))
+    )
+    .select(".domain")
+      .remove(); 
 }
 
 draw()
