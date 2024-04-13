858c4e0aa82abed01fc0222eb9989a2c4f637ea2
shaunso <shaunsom@gmail.com>
2024-04-13 20:25:00 +0200

    Add files via upload

--- ward map/app.js
+++ ward map/app.js
@@ -1,13 +1,13 @@
 async function draw() {
 
   // load the JSON file with the data that draws the map for the provinces and districts
-  const topology = await d3.json("p_w.json");  
+  const topology = await d3.json("w.json");  
 
   // loading the data retrieved from the .CSV, dependent on the user input
   // variable is called in the next block, which is an if statement
   // default option shall be _population density_
   const data = await d3.csv("wardData.csv");
-  console.log(data)
+
 
   // const administrativeLevel = "district"
   
@@ -19,8 +19,8 @@ async function draw() {
 
   // defining the map projection type
   const mapProjection = d3.geoMercator()
-    .center([20, -5]) 
-    .scale(480)
+    .center([27, -20]) 
+    .scale(4380)
     .rotate([0, 0, -4]); 
 
   // drawing the svg area for where d3 will draw the map
@@ -40,31 +40,27 @@ async function draw() {
       
   // create a map object in order to store the population density per square kilometre per district using the ADM2_PCODE & POPULATION_DENSITY in key:value pairs
   // map will be used to select the colour of each district using the thresholds defined by the colorScale variable
-  const valueMap = new Map(data.map( d => [d.ADM3_PCODE, +d3.format(".1f")(d.POPULATION)]));
+  const valueMap = new Map(data.map( d => [d.ADM3_PCODE, +d3.format(".1f")(d.POPULATION_DENSITY)]));
+  console.log(valueMap)
   
-  
-  // storing the area of each district in a separate array to be used to assign each district a colour as defined by the thresholds in the colour scale
-  const areaData = data.map( d => +d3.format(".1f")(d.POPULATION));
+  // defining the domain and colour scale
+  const domain = [0.1, Math.max(...data.map(d => d.POPULATION_DENSITY))];
 
-  // defining the domain and range
-  // discrete input and discrete output where the bands for the domain are varying, hence d3.scaleThreshold() is used
-  const colorScale = d3.scaleQuantile()
-  .domain(areaData)
-  .range(d3.schemeBlues[9]);
+  const color =  d3.scaleLog()
+      .base(2)
+      .domain(domain)
+      .interpolate(() => d3.interpolatePlasma)
 
   // drawing the map on the web page
   chart.selectAll("path")
     .data(topojson.feature(topology, topology.objects.ward).features)
     .join("path")
     .attr("d", pathGenerator)
-    .attr("fill", d => colorScale(valueMap.get(d.properties.ADM3_PCODE)));  
-
-  chart.append("g")
-    .selectAll("path")
-    .data(topojson.feature(topology, topology.objects.province).features)
-    .join("path")
-    .attr("d", pathGenerator)
-    .attr("class", "outline");
+    .attr("fill", d => color(valueMap.get(d.properties.ADM3_PCODE)))
+    .on("click", function(d, i) {
+      console.log(`The population density for ${i.properties.ADM2_EN} ward ${i.properties.ADM3_EN} is ${d3.format(",")(valueMap.get(i.properties.ADM3_PCODE))} people per square kilometer`);
+    }
+    )
 
   ///////////////////////////////////////////////
     // zoom function
@@ -77,16 +73,16 @@ async function draw() {
   // Then we instruct the computer, when it “sees” a “zoom” event, to carry out another function (.on("zoom",function(event) {).
   // 
   // that function firstly selects all our paths g.selectAll('path') and then applies a transform to them based on the event (.attr('tran
-  const zoom = d3.zoom()
-    .scaleExtent([1,4])
-    .on("zoom", function (event) {
-      chart.selectAll("path")
-        .attr("transform", event.transform);
-    });
+// 
+const zoom = d3.zoom()
+.scaleExtent([1, 20])
+.on('zoom', function(event) {
+  chart.selectAll('path')
+    .attr('transform', event.transform)
+});
 
-  // calling the zoom function
-  svg.call(zoom);
-
+// Calling the zoom function.
+svg.call(zoom);
     
 }
 
